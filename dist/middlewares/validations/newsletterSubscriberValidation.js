a84fc64ec5113fca4f0f7cf9e2c0d67209beb839
dushimec515 <dushimec515@gmail.com>
2025-10-12 20:34:50 +0000

    dist deleted

--- dist/middlewares/validations/newsletterSubscriberValidation.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.subscribeValidation = void 0;
-const express_validator_1 = require("express-validator");
-exports.subscribeValidation = [(0, express_validator_1.body)('email').isEmail().withMessage('A valid email is required')];
-//# sourceMappingURL=newsletterSubscriberValidation.js.map
--- dist/middlewares/validations/newsletterValidation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendNewsletterValidation = void 0;
-const express_validator_1 = require("express-validator");
-exports.sendNewsletterValidation = [
-    (0, express_validator_1.body)('title').isString().notEmpty().withMessage('Title is required'),
-    (0, express_validator_1.body)('content').isString().notEmpty().withMessage('Content is required'),
-];
-//# sourceMappingURL=newsletterValidation.js.map
