625112237dd33afe9ec8751ad712b3498fe83427
dushimec <dushimec515@gmail.com>
2025-10-26 17:15:18 +0200

    fixes for reset password validations

--- src/middlewares/validations/forgotPasswordValidator.ts
+++ src/middlewares/validations/forgotPasswordValidator.ts
@@ -3,6 +3,5 @@ import { body } from "express-validator";
 export const forgotPasswordValidator = [body("email").isEmail().withMessage("Please enter a valid email")];
 
 export const resetPasswordValidator = [
-    body("token").notEmpty().withMessage("Token is required"),
     body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
 ];
