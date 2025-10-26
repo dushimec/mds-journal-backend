2b026d82a925f986ed60867233b14b61b9d8216d
dushimec515 <dushimec515@gmail.com>
2025-10-26 13:00:56 +0000

    updates

--- src/routers/swagger.route.ts
+++ src/routers/swagger.route.ts
@@ -1,9 +1,22 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "../config/swagger";
 
 const router = Router();
 
-router.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+router.get("/swagger.json", (req: Request, res: Response) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerSpec);
+});
+
+router.use(
+  "/",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/api/docs/swagger.json",
+    },
+  })
+);
 
 export default router;
